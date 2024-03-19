5c17b44c9497c701e4b04930344324472c243316
KHALID ABDU <khalid11abdu@gmail.com>
2024-03-19 11:38:30 +0300

    structured

---
+++ product.py
@@ -0,0 +1,20 @@
+import unittest
+
+class Product:
+    def __init__(self, name, price, quantity) -> None:
+        
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculateTotal(self):
+        if self.quantity < 0 or self.price < 0:
+            raise ValueError('Price and quantity must be greater than 0')
+        return self.price * self.quantity
+    
+
+
+
+
+
+
